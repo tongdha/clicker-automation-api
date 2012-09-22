c171ef7cbeacc4310ee53e8298830e55cfc12719
Tong Ha <tong.ha@cbsinteractive.com>
2012-09-22 00:20:22 -0700

    Added web code for crawler_configs

--- app/controllers/crawler_configs_controller.rb
+++ app/controllers/crawler_configs_controller.rb
@@ -29,15 +29,12 @@ class CrawlerConfigsController < ApplicationController
   # POST /crawler_configs.json
   def create
     @crawler_config = CrawlerConfig.new(params[:crawler_config])
-
-    respond_to do |format|
-      if @crawler_config.save
-        format.html { redirect_to @crawler_config, notice: 'Crawler config was successfully created.' }
-        format.json { render json: @crawler_config, status: :created, location: @crawler_config }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @crawler_config.errors, status: :unprocessable_entity }
-      end
+    if @crawler_config.save
+      flash[:notice] = "Crawler Config has been created!"
+      redirect_to @crawler_config
+    else
+      flash[:alert] = "Crawler Config has NOT been created!"
+      render :action => "new"
     end
   end
 
@@ -45,15 +42,13 @@ class CrawlerConfigsController < ApplicationController
   # PUT /crawler_configs/1.json
   def update
     @crawler_config = CrawlerConfig.find(params[:id])
-
-    respond_to do |format|
-      if @crawler_config.update_attributes(params[:crawler_config])
-        format.html { redirect_to @crawler_config, notice: 'Crawler config was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @crawler_config.errors, status: :unprocessable_entity }
-      end
+    
+    if @crawler_config.update_attributes(params[:crawler_config])
+      flash[:notice] = "Crawler Config has been updated!"
+      redirect_to @crawler_config
+    else
+      flash[:alert] = "Crawler Config has NOT been updated!"
+      render :action => "edit"
     end
   end
 
@@ -62,10 +57,9 @@ class CrawlerConfigsController < ApplicationController
   def destroy
     @crawler_config = CrawlerConfig.find(params[:id])
     @crawler_config.destroy
-
-    respond_to do |format|
-      format.html { redirect_to crawler_configs_url }
-      format.json { head :no_content }
-    end
+    
+    @crawler_config.destroy
+    flash[:notice] = "Crawler Config has been deleted!"
+    redirect_to crawler_configs_path
   end
 end
--- app/models/crawler_config.rb
+++ app/models/crawler_config.rb
@@ -1,7 +1,7 @@
 class CrawlerConfig < ActiveRecord::Base
   self.primary_key="crawlerConfigId"
   self.table_name = "crawler_config"
-  attr_accessible :description, :crawlingEnabled, :intervalInHours, :allowedFilters, :useSourceId
+  attr_accessible :description, :crawlingEnabled, :intervalInHours, :allowedFilters, :useSourceId, :startURL, :distributorProfileId, :mediaTypeId, :maxDepth
   
   has_many :crawl_histories, :foreign_key => "crawlerConfigId"
   
